fff87a978ef7e173dd1e8ea54ffa74b58a6336c9
Pablo Rodriguez Albarran <pablorodriguezalbarran2000@gmail.com>
2024-04-24 10:48:21 +0200

    profile/teammembers corrections

--- src/app/components/team/team-members-carousel/team-members-carousel.component.ts
+++ src/app/components/team/team-members-carousel/team-members-carousel.component.ts
@@ -19,13 +19,13 @@ export class TeamMembersCarouselComponent implements OnInit {
     this.teamMembers = this.allTeamMembers.slice(1);
     this.responsiveOptions = [
       {
-        breakpoint: '1400px',
-        numVisible: 3,
+        breakpoint: '1440px',
+        numVisible: 2,
         numScroll: 1,
       },
       {
         breakpoint: '1220px',
-        numVisible: 2,
+        numVisible: 1,
         numScroll: 1,
       },
       {
