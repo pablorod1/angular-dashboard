b700f2450bbafb4183c958b0c63ba734fb293c20
Pablo Rodríguez <pablorodriguez@lucentialab.es>
2024-05-13 14:05:26 +0200

    to do

--- src/app/pages/scrumboard/scrumboard/scrumboard.component.ts
+++ src/app/pages/scrumboard/scrumboard/scrumboard.component.ts
@@ -1,31 +1,210 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ScrumboardService, CardProject } from '../../../services/scrumboard.service';
+import {
+  ScrumboardService,
+  CardProject,
+  Task,
+} from '../../../services/scrumboard.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-scrumboard',
   templateUrl: './scrumboard.component.html',
-  styleUrl: './scrumboard.component.css'
+  styleUrl: './scrumboard.component.css',
+  providers: [ConfirmationService, MessageService],
+  animations: [
+    trigger('filtersAnimation', [
+      state(
+        'inactive',
+        style({
+          transform: 'height: 0%',
+          visibility: 'hidden',
+          opacity: 0,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'height: 100%',
+          opacity: 1,
+          visibility: 'visible',
+        })
+      ),
+      transition('inactive => active', animate('400ms ease-in')),
+      transition('active => inactive', animate('1000ms ease-out')),
+    ]),
+  ],
 })
+
 export class ScrumboardComponent implements OnInit {
   cards!: CardProject[];
   card!: CardProject;
   cardTitle!: string;
+  selectedTask!: Task;
+  tasks!: Task[];
+  showAddTaskDialog: boolean = false;
+  newTask!: Task;
+  filteredTasks!: Task[];
+  searchQuery: string = '';
+  showFilters: boolean = false;
 
   status: string[] = ['To Do', 'In Progress', 'In Review', 'Done'];
+  priority: string[] = ['Low', 'Medium', 'High'];
+  tags: string[] = ['Design', 'Marketing', 'Development', 'Concept'];
 
-  constructor(private route: ActivatedRoute, private scrumboardService: ScrumboardService){}
+  constructor(
+    private route: ActivatedRoute,
+    private scrumboardService: ScrumboardService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
     this.cards = this.scrumboardService.getCardProjects();
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.cardTitle = params['title'];
-      this.card = this.cards.find(card => card.title === this.cardTitle) || {} as CardProject;
+      this.card =
+        this.cards.find((card) => card.title === this.cardTitle) ||
+        ({} as CardProject);
+    });
+    // Inicializar tasks
+    this.tasks = this.card.tasks;
+    this.newTask = {
+      id: 0,
+      title: '',
+      taskImage: '',
+      description: '',
+      status: '',
+      priority: '',
+      dueDate: '',
+      tags: [],
+      users: [],
+    };
+  }
+
+  selectTask(task: Task) {
+    this.selectedTask = task;
+  }
+
+  // Drag and Drop
+  dragStart(task: Task) {
+    this.selectedTask = task;
+  }
+  drop(status: string) {
+    if (this.selectedTask) {
+      // Actualizar Status
+      this.selectedTask.status = status;
+    }
+  }
+  dragEnd() {
+    this.selectedTask = {} as Task;
+  }
+
+  createTask() {
+    this.showAddTaskDialog = true;
+    this.newTask = {
+      id: 0,
+      title: '',
+      taskImage: '',
+      description: '',
+      status: '',
+      priority: '',
+      dueDate: '',
+      tags: [],
+      users: [],
+    };
+  }
+
+  addTask() {
+    this.newTask.id = this.tasks.length + 1;
+    this.newTask.dueDate = this.formatDate(this.newTask.dueDate);
+    console.log(this.newTask);
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to add this task?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.tasks.push(this.newTask);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Task Created',
+          life: 3000,
+        });
+        this.cancelCreateTask();
+        console.log(this.tasks);
+      },
     });
   }
 
+  cancelCreateTask() {
+    this.showAddTaskDialog = false;
+    this.newTask = {
+      id: 0,
+      title: '',
+      taskImage: '',
+      description: '',
+      status: '',
+      priority: '',
+      dueDate: '',
+      tags: [],
+      users: [],
+    };
+  }
+
+  // Filter Tasks by Status
+  getTasksByStatus(status: string): Task[] {
+    if (this.searchQuery === ''){
+      return this.tasks.filter((task) => task.status === status);
+    } else {
+      return this.getFilteredTasks(this.searchQuery).filter((task) => task.status === status);
+    }
+  }
+
+  getFilteredTasks(searchQuery: string): Task[]{
+    return this.tasks.filter((task) => task.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  }
+
+  // Fondo y Color del texto para etiquetas
+  getStatusBackground(status: string): string {
+    switch (status) {
+      case 'To Do':
+        return 'bg-secondary';
+      case 'In Progress':
+        return 'bg-primary ';
+      case 'In Review':
+        return 'bg-danger';
+      case 'Done':
+        return 'bg-success ';
+      default:
+        return '';
+    }
+  }
+
+  getStatusColor(status: string): string {
+    switch (status) {
+      case 'To Do':
+        return 'text-secondary';
+      case 'In Progress':
+        return 'text-primary';
+      case 'In Review':
+        return 'text-danger';
+      case 'Done':
+        return 'text-success';
+      default:
+        return '';
+    }
+  }
+
   getStatusSeverity(status: string): string {
-    switch(status) {
+    switch (status) {
       case 'To Do':
         return 'bg-secondary text-secondary';
       case 'In Progress':
@@ -37,5 +216,69 @@ export class ScrumboardComponent implements OnInit {
       default:
         return '';
     }
+  }
+
+  getPrioritySeverity(priority: string): string {
+    switch (priority) {
+      case 'Low':
+        return 'bg-success text-success';
+      case 'Medium':
+        return 'bg-warning text-warning';
+      case 'High':
+        return 'bg-danger text-danger';
+      default:
+        return '';
+    }
+  }
+  getTagColor(tag: string): string {
+    switch (tag) {
+      case 'Design':
+        return 'bg-success text-success';
+      case 'Marketing':
+        return 'bg-info text-info';
+      case 'Development':
+        return 'bg-warning text-warning';
+      case 'Concept':
+        return 'bg-primary text-primary';
+      default:
+        return '';
+    }
+  }
+  getTagIcon(tag: string): string {
+    switch (tag) {
+      case 'Design':
+        return 'fa-brush';
+      case 'Marketing':
+        return 'fa-bullhorn';
+      case 'Development':
+        return 'fa-code';
+      case 'Concept':
+        return 'fa-lightbulb';
+      default:
+        return '';
+    }
+  }
+
+  formatDate(date: string): string {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    const month = months[d.getMonth()];
+    const day = d.getDate();
+
+    return `${day} ${month} ${year}`;
   }
 }
--- src/app/services/scrumboard.service.ts
+++ src/app/services/scrumboard.service.ts
@@ -13,11 +13,24 @@ export interface Icon{
 }
 
 export interface CardProject{
-  id: string;
+  id: number;
   title: string;
   description: string;
   icon: Icon;
   users: User[];
+  tasks: Task[];
+}
+
+export interface Task{
+  id: number;
+  taskImage?: string;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+  users: User[];
+  tags: string[];
+  dueDate: string;
 }
 
 @Injectable({
@@ -136,7 +149,7 @@ export class ScrumboardService {
 
   cardProjects: CardProject[] = [
     {
-      id: '1',
+      id: 1,
       title: 'Project 1',
       description: 'Project 1 description',
       icon: {
@@ -160,10 +173,184 @@ export class ScrumboardService {
           name: 'Alice',
           imageUrl: '../../../../assets/profile-pictures/profile-picture4.webp'
         }
+      ],
+      tasks: [
+        {
+          id: 1,
+          taskImage: 'assets/city.webp',
+          title: 'Task 1',
+          description: 'Task 1 description',
+          status: 'To Do',
+          dueDate: '28 Jun 2024',
+          priority: 'Low',
+          users: [
+            {
+              id: 1,
+              name: 'John Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture2.webp'
+            },
+            {
+              id: 2,
+              name: 'Jane Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture5.webp'
+            }
+          ],
+          tags: ['Design', 'Marketing']
+        },
+        {
+          id: 2,
+          title: 'Task 2',
+          description: 'Task 2 description',
+          status: 'In Progress',
+          dueDate: '15 Jul 2024',
+          priority: 'High',
+          users: [
+            {
+              id: 3,
+              name: 'Alice',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture6.webp'
+            },
+            {
+              id: 4,
+              name: 'Bob',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture7.webp'
+            }
+          ],
+          tags: ['Development', 'Concept']
+        },
+        {
+          id: 3,
+          taskImage: 'assets/bridge_wallpaper.webp',
+          title: 'Task 3',
+          description: 'Task 3 description',
+          status: 'In Review',
+          dueDate: '10 Aug 2024',
+          priority: 'Medium',
+          users: [
+            {
+              id: 5,
+              name: 'Charlie',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture8.webp'
+            },
+            {
+              id: 6,
+              name: 'David',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture9.webp'
+            }
+          ],
+          tags: ['Design', 'Development']
+        },
+        {
+          id: 4,
+          taskImage: 'assets/car_vacations.webp',
+          title: 'Task 4',
+          description: 'Task 4 description',
+          status: 'Done',
+          dueDate: '13 May 2024',
+          priority: 'Low',
+          users: [
+            {
+              id: 7,
+              name: 'Eve',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture10.webp'
+            },
+            {
+              id: 8,
+              name: 'Frank',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture.webp'
+            }
+          ],
+          tags: ['Concept', 'Marketing']
+        },
+        {
+          id: 5,
+          title: 'Task 5',
+          description: 'Task 5 description',
+          status: 'To Do',
+          dueDate: '5 Jun 2024',
+          priority: 'High',
+          users: [
+            {
+              id: 1,
+              name: 'John Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture2.webp'
+            },
+            {
+              id: 2,
+              name: 'Jane Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture5.webp'
+            }
+          ],
+          tags: ['Design', 'Development']
+        },
+        {
+          id: 6,
+          title: 'Task 6',
+          description: 'Task 6 description',
+          taskImage: 'assets/river.webp',
+          status: 'In Progress',
+          dueDate: '15 Jul 2024',
+          priority: 'Medium',
+          users: [
+            {
+              id: 3,
+              name: 'Alice',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture3.webp'
+            },
+            {
+              id: 4,
+              name: 'Bob',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture4.webp'
+            }
+          ],
+          tags: ['Development', 'Concept']
+        },
+        {
+          id: 7,
+          title: 'Task 7',
+          description: 'Task 7 description',
+          status: 'In Review',
+          dueDate: '10 Aug 2024',
+          priority: 'Low',
+          users: [
+            {
+              id: 5,
+              name: 'Charlie',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture5.webp'
+            },
+            {
+              id: 6,
+              name: 'David',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture6.webp'
+            }
+          ],
+          tags: ['Design', 'Marketing']
+        },
+        {
+          id: 8,
+          title: 'Task 8',
+          description: 'Task 8 description',
+          status: 'Done',
+          dueDate: '13 May 2024',
+          priority: 'High',
+          users: [
+            {
+              id: 7,
+              name: 'Eve',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture7.webp'
+            },
+            {
+              id: 8,
+              name: 'Frank',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture8.webp'
+            }
+          ],
+          tags: ['Marketing', 'Design']
+        }
       ]
     },
     {
-      id: '2',
+      id: 2,
       title: 'Project 2',
       description: 'Project 2 description',
       icon: {
@@ -202,10 +389,96 @@ export class ScrumboardService {
           name: 'David',
           imageUrl: '../../../../assets/profile-pictures/profile-picture9.webp'
         }
+      ],
+      tasks: [
+        {
+          id: 1,
+          title: 'Task 1',
+          description: 'Task 1 description',
+          status: 'To Do',
+          dueDate: '5 Jun 2024',
+          priority: 'High',
+          users: [
+            {
+              id: 1,
+              name: 'John Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture.webp'
+            },
+            {
+              id: 2,
+              name: 'Jane Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture2.webp'
+            }
+          ],
+          tags: ['Design', 'Development']
+        },
+        {
+          id: 2,
+          title: 'Task 2',
+          description: 'Task 2 description',
+          status: 'In Progress',
+          dueDate: '',
+          priority: 'Medium',
+          users: [
+            {
+              id: 3,
+              name: 'Alice',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture3.webp'
+            },
+            {
+              id: 4,
+              name: 'Bob',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture4.webp'
+            }
+          ],
+          tags: ['Development', 'Concept']
+        },
+        {
+          id: 3,
+          title: 'Task 3',
+          description: 'Task 3 description',
+          status: 'In Review',
+          dueDate: '',
+          priority: 'Low',
+          users: [
+            {
+              id: 5,
+              name: 'Charlie',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture5.webp'
+            },
+            {
+              id: 6,
+              name: 'David',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture6.webp'
+            }
+          ],
+          tags: ['Design', 'Marketing']
+        },
+        {
+          id: 4,
+          title: 'Task 4',
+          description: 'Task 4 description',
+          status: 'Done',
+          dueDate: '',
+          priority: 'High',
+          users: [
+            {
+              id: 7,
+              name: 'Eve',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture7.webp'
+            },
+            {
+              id: 8,
+              name: 'Frank',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture8.webp'
+            }
+          ],
+          tags: ['Marketing', 'Design']
+        }
       ]
     },
     {
-      id: '3',
+      id: 3,
       title: 'Project 3',
       description: 'Project 3 description',
       icon: {
@@ -229,6 +502,92 @@ export class ScrumboardService {
           name: 'Alice',
           imageUrl: '../../../../assets/profile-pictures/profile-picture10.webp'
         }
+      ],
+      tasks: [
+        {
+          id: 1,
+          title: 'Task 1',
+          description: 'Task 1 description',
+          status: 'To Do',
+          dueDate: '',
+          priority: 'High',
+          users: [
+            {
+              id: 1,
+              name: 'John Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture2.webp'
+            },
+            {
+              id: 2,
+              name: 'Jane Doe',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture5.webp'
+            }
+          ],
+          tags: ['Tag 1', 'Tag 2']
+        },
+        {
+          id: 2,
+          title: 'Task 2',
+          description: 'Task 2 description',
+          status: 'In Progress',
+          dueDate: '',
+          priority: 'Low',
+          users: [
+            {
+              id: 3,
+              name: 'Alice',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture6.webp'
+            },
+            {
+              id: 4,
+              name: 'Bob',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture7.webp'
+            }
+          ],
+          tags: ['Tag 3', 'Tag 4']
+        },
+        {
+          id: 3,
+          title: 'Task 3',
+          description: 'Task 3 description',
+          status: 'In Review',
+          dueDate: '',
+          priority: 'Medium',
+          users: [
+            {
+              id: 5,
+              name: 'Charlie',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture8.webp'
+            },
+            {
+              id: 6,
+              name: 'David',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture9.webp'
+            }
+          ],
+          tags: ['Tag 5', 'Tag 6']
+        },
+        {
+          id: 4,
+          title: 'Task 4',
+          description: 'Task 4 description',
+          status: 'Done',
+          dueDate: '',
+          priority: 'Low',
+          users: [
+            {
+              id: 7,
+              name: 'Eve',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture10.webp'
+            },
+            {
+              id: 8,
+              name: 'Frank',
+              imageUrl: '../../../../assets/profile-pictures/profile-picture.webp'
+            }
+          ],
+          tags: ['Tag 7', 'Tag 8']
+        }
       ]
     },
   ]
@@ -243,6 +602,11 @@ export class ScrumboardService {
 
   getIcons(){
     return this.icons;
+  }
+
+  getTasks(){
+    return this.cardProjects.map(project => project.tasks).flat();
+
   }
 
   addCardProject(card: CardProject){
